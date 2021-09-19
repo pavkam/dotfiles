724870b1a232b86be5247624c95b79c150634ffe
Alexandru Ciobanu <alex+git@ciobanu.org>
2021-09-19 19:28:10 +0100

    feat: many new improvements including package installation.

--- .zshrc
+++ .zshrc
@@ -1,12 +1,23 @@
 # ---------------------------------------------------------------------------
 # | pavkam's zsh setup. Nothing interesting to see here, please move along! |
+# | Parts of this file are based on "manjaro-zsh-config" and others parts,  |
+# | are based on many samples found all over the Internet!                  |
+# |                                                                         |
+# | I apologize in advance if I did not mention the sources explicitly      |
 # ---------------------------------------------------------------------------
-
+                                                                       
 # Oh-My-Zsh Setup.
 export ZSH="$HOME/.oh-my-zsh"
 
-plugins=( git z fzf fd docker )
-export ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=( git z fzf fd docker zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting )
+                                            
+COMM=$(basename "$(cat "/proc/$PPID/comm")")
+if [ "$COMM" = "login" ] || [ "$TERM" = "linux" ]; then
+  export ZSH_THEME="clean"    
+else
+  export ZSH_THEME="powerlevel10k/powerlevel10k"   
+fi
+
 source $ZSH/oh-my-zsh.sh
 
 P10K_INIT="$HOME/.zshrc.p10k"
@@ -18,8 +29,180 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Other settings
+# Other settings and general options
 export EDITOR='vim'
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+
+setopt correct                                                  
+setopt extendedglob
+setopt nocaseglob
+setopt rcexpandparam
+setopt nocheckjobs
+setopt numericglobsort
+setopt nobeep
+setopt appendhistory
+setopt histignorealldups
+setopt autocd
+setopt inc_append_history
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' rehash true
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+
+HISTFILE=~/.zhistory
+HISTSIZE=10000
+SAVEHIST=10000
+WORDCHARS=${WORDCHARS//\/[&.;]}
+
+# Keys 
+bindkey -e
+bindkey '^[[7~' beginning-of-line
+bindkey '^[[H' beginning-of-line
+
+if [[ "${terminfo[khome]}" != "" ]]; then
+  bindkey "${terminfo[khome]}" beginning-of-line
+fi
+
+bindkey '^[[8~' end-of-line
+bindkey '^[[F' end-of-line
+
+if [[ "${terminfo[kend]}" != "" ]]; then
+  bindkey "${terminfo[kend]}" end-of-line
+fi
+
+bindkey '^[[2~' overwrite-mode
+bindkey '^[[3~' delete-char
+bindkey '^[[C'  forward-char
+bindkey '^[[D'  backward-char
+bindkey '^[Oc' forward-word
+bindkey '^[Od' backward-word
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
+bindkey '^H' backward-kill-word
+
+# Color man pages
+export LESS_TERMCAP_mb=$'\E[01;32m'
+export LESS_TERMCAP_md=$'\E[01;32m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;47;34m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;36m'
+export LESS=-R
+
+# bind UP and DOWN arrow keys to history substring search
+zmodload zsh/terminfo
+
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up			
+bindkey '^[[B' history-substring-search-down
+
+# Arch/Manjaro-specific feaure. Offer to install missing package if command is not found.
+if [[ -r /usr/share/zsh/functions/command-not-found.zsh ]]; then
+    source /usr/share/zsh/functions/command-not-found.zsh
+    export PKGFILE_PROMPT_INSTALL_MISSING=1
+fi
+
+function title {
+  emulate -L zsh
+  setopt prompt_subst
+
+  [[ "$EMACS" == *term* ]] && return
+
+  # if $2 is unset use $1 as default
+  # if it is set and empty, leave it as is
+  : ${2=$1}
+
+  case "$TERM" in
+    xterm*|putty*|rxvt*|konsole*|ansi|mlterm*|alacritty|st*)
+      print -Pn "\e]2;${2:q}\a" # set window name
+      print -Pn "\e]1;${1:q}\a" # set tab name
+      ;;
+    screen*|tmux*)
+      print -Pn "\ek${1:q}\e\\" # set screen hardstatus
+      ;;
+    *)
+    # Try to use terminfo to set the title
+    # If the feature is available set title
+    if [[ -n "$terminfo[fsl]" ]] && [[ -n "$terminfo[tsl]" ]]; then
+      echoti tsl
+      print -Pn "$1"
+      echoti fsl
+    fi
+      ;;
+  esac
+}
+
+ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+ZSH_THEME_TERM_TITLE_IDLE="%n@%m:%~"
+
+# Runs before showing the prompt
+function mzc_termsupport_precmd {
+  [[ "${DISABLE_AUTO_TITLE:-}" == true ]] && return
+  title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
+}
+
+# Runs before executing the command
+function mzc_termsupport_preexec {
+  [[ "${DISABLE_AUTO_TITLE:-}" == true ]] && return
+
+  emulate -L zsh
+
+  # split command into array of arguments
+  local -a cmdargs
+  cmdargs=("${(z)2}")
+  # if running fg, extract the command from the job description
+  if [[ "${cmdargs[1]}" = fg ]]; then
+    # get the job id from the first argument passed to the fg command
+    local job_id jobspec="${cmdargs[2]#%}"
+    # logic based on jobs arguments:
+    # http://zsh.sourceforge.net/Doc/Release/Jobs-_0026-Signals.html#Jobs
+    # https://www.zsh.org/mla/users/2007/msg00704.html
+    case "$jobspec" in
+      <->) # %number argument:
+        # use the same <number> passed as an argument
+        job_id=${jobspec} ;;
+      ""|%|+) # empty, %% or %+ argument:
+        # use the current job, which appears with a + in $jobstates:
+        # suspended:+:5071=suspended (tty output)
+        job_id=${(k)jobstates[(r)*:+:*]} ;;
+      -) # %- argument:
+        # use the previous job, which appears with a - in $jobstates:
+        # suspended:-:6493=suspended (signal)
+        job_id=${(k)jobstates[(r)*:-:*]} ;;
+      [?]*) # %?string argument:
+        # use $jobtexts to match for a job whose command *contains* <string>
+        job_id=${(k)jobtexts[(r)*${(Q)jobspec}*]} ;;
+      *) # %string argument:
+        # use $jobtexts to match for a job whose command *starts with* <string>
+        job_id=${(k)jobtexts[(r)${(Q)jobspec}*]} ;;
+    esac
+
+    # override preexec function arguments with job command
+    if [[ -n "${jobtexts[$job_id]}" ]]; then
+      1="${jobtexts[$job_id]}"
+      2="${jobtexts[$job_id]}"
+    fi
+  fi
+
+  # cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD=${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
+  local LINE="${2:gs/%/%%}"
+
+  title '$CMD' '%100>...>$LINE%<<'
+}
+
+autoload -U add-zsh-hook
+
+add-zsh-hook precmd mzc_termsupport_precmd
+add-zsh-hook preexec mzc_termsupport_preexec
+
+# FZF-ness
 
 FD_OPTIONS="--follow --exclude .git --exclude node_modules"
 export FZF_COMPLETION_TRIGGER='**'
@@ -65,6 +248,11 @@ z() {
     [ $# -gt 0 ] && _z "$*" && return
     cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
 }
+
+alias cp='cp -i'
+alias df='df -h'
+alias free='free -m'
+alias yolo='git add . && git commit && git push'
 
 SDKMAN_DIR="$HOME/.sdkman"
 if [ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # ---------------------------------------------------------------------------
-# | pavkam's zsh setup. Nothing interesting to see here, please move along! |
+# | pavkam's .dotfiles installation script.                                 |
+# |                                                                         |
+# | No warranties are provided, no responsability is taken. You're on your  |
+# | own! I will recommend you backup your home before trying this script.   |
 # ---------------------------------------------------------------------------
+
+# Logging.
+LOG_FILE=~/.dotfiles.log
+echo -ne "" > ~/.dotfiles.log 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Unable to create a log file in your home directory. This is a minimum requirement."
+    exit 1
+fi
 
 # "Die" handling. Wil be used to exit the shell when requested by child script (functions).
 trap "exit 1" TERM
@@ -11,7 +22,7 @@ DFN=`realpath $0`
 DF=`dirname $DFN`
 
 function die() {
-    tput setaf 1 && echo "Terminating due to an error!"
+    echo "Terminating due to an error!"
 
     if [ $$ -eq $TOP_PID ]; then
         exit 1
@@ -20,31 +31,64 @@ function die() {
     fi
 }
 
+# Colors and output.
+RED=`tput setaf 1`
+BROWN=`tput setaf 3`
+NORMAL=`tput sgr0`
+CLEARRET="\r`tput el`"
+ROLLING=0
+
 function whoops() {
-    >&2 tput setaf 1 && echo "[WHOOPS] $1"
+    if [ $ROLLING -eq 1 ]; then
+        echo
+    fi
+
+    echo "Whoops: $1" >> $LOG_FILE
+    echo "$RED      Whoops: $1"
     die
 }
  
 function err() {
-    >&2 tput setaf 1 && echo "[ERR] $1"   
+    if [ $ROLLING -eq 1 ]; then
+        echo
+    fi
+
+    echo "Error: $1" >> $LOG_FILE
+    echo "$RED      Error: $1"
     die
 }
 
 function warn() {
-    >&2 tput setaf 3 && echo "[WARN] $1"   
-}
+    if [ $ROLLING -eq 1 ]; then
+        echo
+    fi
 
+    echo "Warning: $1" >> $LOG_FILE
+    echo "$BROWN    Warning: $1"
+    ROLLING=0
+}
 
 function info() {
-    tput sgr0 && echo "$1"   
+    if [ $ROLLING -eq 1 ]; then
+        echo
+    fi
+
+    echo "$1" >> $LOG_FILE
+    echo "${NORMAL}$1"   
+    ROLLING=0
 }
 
+function roll() {
+    echo "$1" >> $LOG_FILE
+    echo -ne "${CLEARRET}${NORMAL}$1"
+    ROLLING=1
+}
 
 function link() {
     FROM=$1
     TO=$2
     if [ "$FROM" = "" ]; then
-        whoops "LINK: Expected at least one  argument to function."
+        whoops "Expected at least one  argument to function."
     fi
 
     if [ "$TO" == "" ]; then
@@ -52,33 +96,33 @@ function link() {
     fi
 
     if [ ! -e "$DF/$FROM" ]; then
-        whoops "LINK: The file or directory '$DF/$FROM' does not exist."    
+        whoops "The file or directory '$DF/$FROM' does not exist."    
     fi
 
-    info "LINK: Creating a symlink from '$DF/$FROM' to '$HOME/$TO'..."
+    roll "Creating a symlink from '$DF/$FROM' to '$HOME/$TO'..."
 
     HAS_BACKUP=0
     if [ -e "$HOME/$TO" ]; then
-        warn "LINK: The file or directory '$HOME/$TO' already exists. Backing up and replacing..."
+        warn "The file or directory '$HOME/$TO' already exists. Backing up and replacing..."
 
         rm -r -f -d "$HOME/$TO.old" >/dev/null 2>/dev/null
         mv "$HOME/$TO" "$HOME/$TO.old" >/dev/null 2>/dev/null
         if [ $? -ne 0 ]; then
-            err "LINK: Failed to backup file '$HOME/$TO'."
+            err "Failed to backup file '$HOME/$TO'."
         fi
 
         HAS_BACKUP=1
     fi
 
-    DIR_OF_TO="`dirname -- '$HOME/$TO'`"
+    DIR_OF_TO=`dirname -- "$HOME/$TO"`
     if [ ! -d "$DIR_OF_TO" ]; then
-        info "LINK: The destination directory '$DIR_OF_TO' does not exist. Creating..."
+        roll "The destination directory '$DIR_OF_TO' does not exist. Creating..."
         mkdir -p "$DIR_OF_TO" >/dev/null 2>/dev/null
         if [ $? -ne 0 ]; then
-            err "LINK: Failed to create the destination directory '$DIR_OF_TO'."
+            err "Failed to create the destination directory '$DIR_OF_TO'."
         fi
 
-        info "LINK: Created the destination directory '$DIR_OF_TO'."
+        roll "Created the destination directory '$DIR_OF_TO'."
     fi
 
     ln -s "$DF/$FROM" "$HOME/$TO" 2>/dev/null
@@ -91,34 +135,125 @@ function link() {
             fi
         fi
 
-        err "LINK: Symlink between '$DF/$FROM' and '$HOME/$TO' failed."
+        err "Symlink between '$DF/$FROM' and '$HOME/$TO' failed."
     else
-        info "LINK: Symlink between '$DF/$FROM' and '$HOME/$TO' created."
+        roll "Symlink between '$DF/$FROM' and '$HOME/$TO' created."
     fi
 }
 
 function check_installed() {
     if [ "$1" = "" ]; then
-        whoops "DEP: Expected one argument to function."
+        whoops "Expected one argument to function."
     fi
 
     which "$1" >/dev/null 2>/dev/null
     if [ $? -ne 0 ] && [ ! -f "$1" ]; then
-        warn "DEP: Command '$1' not found."
+        warn "Command '$1' not found."
         return 1
     else
-        info "DEP: Command '$1' has been found."
+        roll "Command '$1' has been found."
         return 0
     fi    
 }
 
-info "Welcome to pavkam's .dotfiles installer. Hope you enjoy the process."
-info "This installer comes with absolutely no guarantees!"
-info 
+function pull_or_clone_repo() {
+    if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ]; then
+        whoops "Expected three arguments to function."
+    fi
+
+    NAME=$1
+    DIR=$2
+    URL=$3
+
+    roll "Installing $NAME using git..."   
+    if [ -d "$DIR" ]; then
+        warn "$NAME is already installed. Pulling..."
+        (
+            cd "$DIR"
+            git pull >/dev/null 2>/dev/null
+            if [ $? -ne 0 ]; then
+                err "Failed to update $NAME git repository."
+            else
+                roll "Updated the $NAME git repository to the latest version."
+            fi
+    )
+    else
+        roll "Cloning $NAME from '$URL'..."
+        git clone "$URL" "$DIR" >/dev/null 2>/dev/null
+        if [ $? -ne 0 ]; then
+            err "Failed to clone $NAME git repository."
+        fi
+
+        roll "Cloned the $NAME repository into '$DIR'."
+    fi
+}
+
+function is_package_installed() {
+    if [ "$1" = "" ]; then
+        whoops "Expected one arguments to function."
+    fi
+
+    roll "Checking if package '$1' is installed..."
+    pacman -Q "$1" >/dev/null 2>/dev/null
+    if [ $? -ne 0 ]; then
+        roll "Package '$1' not installed."
+        return 1
+    else
+        roll "Package '$1' is already installed."
+        return 0
+    fi
+}
+
+ function install_packages() {
+    if [ "$1" = "" ]; then
+        whoops "Expected one arguments to function."
+    fi
+
+    warn "Installing packages ['$1']..."
+    sudo pacman -S --noconfirm $1 >/dev/null 2>/dev/null
+    if [ $? -ne 0 ]; then
+        err "Failed to install one or more packages from the list ['$1']."
+    fi
+}
+ 
+info ".----------------------------------------------------------------------."
+info "| Welcome to pavkam's .dotfiles installer. Hope you enjoy the proces!  |"
+info "| This installer will perform the following changes:                   |"
+info "|     *   Installs dependencies (on Arch-based distros),               |"
+info "|     *   Configures zsh, oh-my-zsh and plugins,                       |"
+info "|     *   Configures git and its settings                              |"
+info "|     *   Confugures vim, Vundle and plugins                           |"
+info ":----------------------------------------------------------------------:"
+info "| ${RED}WARNING: This installer comes with absolutely no guarantees!${NORMAL}         |"
+info "| ${RED}Please backup your home directory for safety reasons.${NORMAL}                |"
+info "------------------------------------------------------------------------"
+info
 
 # Check dependencies (commands that should be installed)
-info "DEP: Checking dependencies..."
-DEPS=( git vim zsh fzf fd "$HOME/.nvm/nvm.sh" mc gcc java diff make less rg sed bat head chsh go node npm pyenv )
+roll "Checking dependencies..."
+
+PACKS=(
+    yay zsh vim git fd mc make diffutils less ripgrep sed bat util-linux nodejs npm nvm pyenv tree gcc go automake binutils bc 
+    bash bzip2 cmake coreutils curl cython dialog docker htop llvm lua lz4 mono perl pyenv python python2 ruby wget 
+    zip dotnet-runtime dotnet-sdk
+)
+
+TO_INSTALL=""
+for i in "${PACKS[@]}"
+do
+    is_package_installed $i
+    if [ $? -ne 0 ]; then
+        TO_INSTALL="$TO_INSTALL $i"
+    fi
+done
+
+if [ "$TO_INSTALL" != "" ]; then
+    install_packages "$TO_INSTALL"
+fi
+
+# ...
+
+DEPS=( git vim zsh fzf fd "$HOME/.nvm/nvm.sh" mc gcc java diff make less rg sed bat head chsh go node npm pyenv tree )
 MUST_DIE=0
 for i in "${DEPS[@]}"
 do
@@ -129,73 +264,32 @@ do
 done
 
 if [ $MUST_DIE -eq 1 ]; then
-    warn "DEP: Please install the missing dependencies before continuing the installation!"
+    warn "Please install the missing dependencies before continuing the installation!"
     die
 else
-    info "DEP: All dependencies are installed."
+    roll "All dependencies are installed."
 fi 
 
 # Setup the shell & Oh-My-Zsh
+check_or_install_zsh
 
-info "SHELL: Checking the current shell..."
-SHELL_NAME=`basename -- $SHELL`
-if [ "$SHELL_NAME" != "zsh" ]; then
-    warn "SHELL: The current shell ('$SHELL_NAME') is not zsh. Setting zsh as default..."
-    chsh -s $(which zsh)
-    if [ $? -ne 0 ]; then
-        err "SHELL: Shell switching failed!"
-    fi
-else
-    info "SHELL: Nothing to do - current shell is already set to zsh."
-fi
+OHMYZSH_DIR=${ZSH:-$HOME/.oh-my-zsh}
+pull_or_clone_repo "Oh-My-Zsh" "$OHMYZSH_DIR" "https://github.com/ohmyzsh/ohmyzsh.git" 
 
-OMZ_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh}
-info "SHELL: Installing Oh-My-Zsh..."   
-if [ -d "$OMZ_DIR" ]; then
-    warn "SHELL: Oh-My-Zsh already installed. Pulling..."
-    (
-        cd "$OMZ_DIR"
-        git pull >/dev/null 2>/dev/null
-        if [ $? -ne 0 ]; then
-            err "SHELL: Failed to update Oh-My-Zsh git repository."
-        else
-            info "SHELL: Updated the Oh-My-Zsh git repository to the latest version."
-        fi
-    )
-else
-    info "SHELL: Cloning Oh-My-Zsh from GitHub..."
-    git clone "https://github.com/ohmyzsh/ohmyzsh.git" "$OMZ_DIR" >/dev/null 2>/dev/null
-    if [ $? -ne 0 ]; then
-        err "SHELL: Failed to clone Oh-My-Zsh git repository."
-    fi
+POWERLEVEL_10K_DIR=${ZSH_CUSTOM:-$OHMYZSH_DIR/custom}/themes/powerlevel10k
+pull_or_clone_repo "Power-level 10K" "$POWERLEVEL_10K_DIR" "https://github.com/romkatv/powerlevel10k.git" 
 
-    info "SHELL: Cloned the Oh-My-Zsh git repository."
-fi
+ZSH_AUTOSUGGESTIONS_DIR=${ZSH_CUSTOM:-$OHMYZSH_DIR/custom}/plugins/zsh-autosuggestions
+pull_or_clone_repo "Zsh Auto-Suggestions" "$ZSH_AUTOSUGGESTIONS_DIR" "https://github.com/zsh-users/zsh-autosuggestions"                                                           
 
-POWERLEVEL_10K_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-if [ -d "$POWERLEVEL_10K_DIR" ]; then
-    warn "SHELL: PowerLevel10K theme already installed. Pulling..."
-    (
-        cd "$POWERLEVEL_10K_DIR"
-        git pull >/dev/null 2>/dev/null
-        if [ $? -ne 0 ]; then
-            err "SHELL: Failed to update PowerLevel10K git repository."
-        else
-            info "SHELL: Updated the PowerLevel10K git repository to the latest version."
-        fi
-    )
-else
-    info "SHELL: Cloning PowerLevel10K from GitHub..."
-    git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "$POWERLEVEL_10K_DIR" >/dev/null 2>/dev/null
-    if [ $? -ne 0 ]; then
-        err "SHELL: Failed to clone PowerLevel10K git repository."
-    fi
+ZSH_HISTORY_SUBSTRING_SEARCH_DIR=${ZSH_CUSTOM:-$OHMYZSH_DIR/custom}/plugins/zsh-history-substring-search
+pull_or_clone_repo "Zsh History Substring Search" "$ZSH_HISTORY_SUBSTRING_SEARCH_DIR" "https://github.com/zsh-users/zsh-history-substring-search"                                                           
 
-    info "SHELL: Cloned the PowerLevel10K git repository."
-fi
+ZSH_SYNTAX_HIGHLIGHTING_DIR=${ZSH_CUSTOM:-$OHMYZSH_DIR/custom}/plugins/zsh-syntax-highlighting
+pull_or_clone_repo "Zsh Syntax Highlighting" "$ZSH_SYNTAX_HIGHLIGHTING_DIR" "https://github.com/zsh-users/zsh-syntax-highlighting"                                                           
 
 # Create all the symlinks
-info "LINK: Creating all symlinks..."
+roll "Creating all symlinks..."
 link .dir_colors
 link .gitconfig
 link .gitignore.global
@@ -221,38 +315,23 @@ link .config/Code/User/settings.json
 link .config/Code/User/settings.json "./.config/Code - OSS/User/settings.json"
 
 # Setup vim
-info "VIM: Setting up vim..."
+roll "Setting up vim..."
 VUNDLE_DIR=$HOME/.vim/bundle/Vundle.vim
-if [ -d "$VUNDLE_DIR" ]; then
-    warn "VIM: Vundle repository already installed. Pulling..."
-    (
-        cd $VUNDLE_DIR
-        git pull >/dev/null 2>/dev/null
-        if [ $? -ne 0 ]; then
-            err "VIM: Failed to update the Vundle git repository."
-        fi
-    )
-else
-    info "VIM: Cloning Vundle from GitHub..."
-    git clone "https://github.com/VundleVim/Vundle.vim.git" "$VUNDLE_DIR" >/dev/null 2>/dev/null
-    if [ $? -ne 0 ]; then
-        err "VIM: Failed to clone Vundle git repository."
-    fi
-fi
+pull_or_clone_repo "Vundle" "$VUNDLE_DIR" "https://github.com/VundleVim/Vundle.vim.git"
 
-info "VIM: Vundle is installed and at the latest version. Installing plugins..."
+roll "Vundle is installed and at the latest version. Installing plugins..."
 vim +PluginInstall +qall >/dev/null 2>/dev/null
 if [ $? -ne 0 ]; then
-    err "VIM: Failed to install Vundle plugins."
+    err "Failed to install Vundle plugins."
 fi
 
 vim +PluginUpdate +qall >/dev/null 2>/dev/null
 if [ $? -ne 0 ]; then
-    err "VIM: Failed to update Vundle plugins."
+    err "Failed to update Vundle plugins."
 fi
 
-info "VIM: All Vundle plugins have been installed & updated."
-info "VIM: Building YouCompleteMe plugin..."
+roll "All Vundle plugins have been installed & updated."
+roll "Building YouCompleteMe plugin..."
 (
     cd ~/.vim/bundle/youcompleteme
     ./install.sh >/dev/null 2>/dev/null
@@ -261,4 +340,4 @@ info "VIM: Building YouCompleteMe plugin..."
     fi
 )
 
-info "All done! You're good to go!"
+roll "All done! You're good to go!"
