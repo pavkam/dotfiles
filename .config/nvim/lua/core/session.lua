1514359b9ff58c08f3d525b76a026a6443dc9eb2
Alexandru Ciobanu <alex+git@ciobanu.org>
2024-05-04 00:17:30 +0100

    feat: better session management

--- .config/nvim/lua/core/session.lua
+++ .config/nvim/lua/core/session.lua
@@ -1,12 +1,17 @@
 local utils = require 'core.utils'
 
+---@class core.session
 local M = {}
 local session_dir = utils.join_paths(vim.fn.stdpath 'data', 'sessions')
+local current_session_name
 
-function get_session_file_prefix()
+--- Get the current session name
+---@return string
+local function get_session_name()
     local name = vim.fn.getcwd()
     local branch = vim.trim(vim.fn.system 'git branch --show-current')
 
+    ---@type string
     local full_name
 
     if vim.v.shell_error == 0 then
@@ -21,48 +26,78 @@ function get_session_file_prefix()
     end)
     full_name = string.gsub(full_name, ' ', '+')
 
-    return utils.join_paths(session_dir, full_name)
+    local res = utils.join_paths(session_dir, full_name)
+    ---@cast res string
+
+    return res
 end
 
-function M.get_session_file_name()
-    return get_session_file_prefix() .. '.vim'
+--- Save the current session
+---@param name string # the name of the session
+function M.save_session(name)
+    vim.defer_fn(function()
+        vim.fn.mkdir(session_dir, 'p')
+
+        vim.cmd('mks! ' .. name .. '.vim')
+        vim.cmd('wshada! ' .. name .. '.shada')
+    end, 0)
 end
 
-function M.get_shada_file_name()
-    return get_session_file_prefix() .. '.shada'
-end
+--- Restore a session
+---@param name string # the name of the session
+function M.restore_session(name)
+    vim.defer_fn(function()
+        -- close all windows, tabs and buffers
+        vim.cmd [[silent! tabonly!]]
+        vim.cmd [[silent! %bd!]]
+        vim.cmd [[silent! %bw!]]
 
-function M.save_session()
-    vim.fn.mkdir(session_dir, 'p')
-    vim.cmd('mks! ' .. M.get_session_file_name())
-    vim.cmd('wshada! ' .. M.get_shada_file_name())
-end
+        require('symbol-usage').refresh()
 
-function M.restore_session()
-    local session_file_name = M.get_session_file_name()
-    local shada_file_name = M.get_shada_file_name()
+        -- restore session and shada files
+        local session_file_name = name .. '.vim'
+        local shada_file_name = name .. '.shada'
 
-    if vim.fn.filereadable(session_file_name) == 1 then
-        vim.cmd('source ' .. session_file_name)
-    end
-    if vim.fn.filereadable(shada_file_name) == 1 then
-        vim.cmd('rshada ' .. shada_file_name)
-    end
+        if vim.fn.filereadable(session_file_name) == 1 then
+            vim.cmd('source ' .. session_file_name)
+        end
+        if vim.fn.filereadable(shada_file_name) == 1 then
+            vim.cmd('rshada ' .. shada_file_name)
+        end
+    end, 0)
 end
 
 utils.on_event('VimLeavePre', function()
-    M.save_session()
+    M.save_session(get_session_name())
 end)
 
 utils.on_event('User', function()
-    vim.defer_fn(M.restore_session, 0)
+    current_session_name = get_session_name()
+    M.restore_session(current_session_name)
 end, 'LazyDone')
 
+utils.on_event({ 'FocusGained', 'TermClose', 'TermLeave' }, function()
+    local new_session_name = get_session_name()
+    if current_session_name ~= new_session_name then
+        M.save_session(current_session_name)
+        M.restore_session(new_session_name)
+
+        current_session_name = new_session_name
+    end
+end)
+
 vim.api.nvim_create_user_command('SessionSave', function()
-    M.save_session()
+    M.save_session(current_session_name)
     utils.info 'Session saved'
 end, {
     desc = 'Save session',
+})
+
+vim.api.nvim_create_user_command('SessionRestore', function()
+    M.restore_session(current_session_name)
+    utils.info 'Session restored'
+end, {
+    desc = 'Restore session',
 })
 
 return M
