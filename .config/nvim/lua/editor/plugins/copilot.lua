3bbb695f58c94ccda5253ba3fd2315115acdd8ad
Alexandru Ciobanu <alex+git@ciobanu.org>
2024-06-23 21:13:45 +0100

    feat: updates to pluginsd

--- .config/nvim/lua/editor/plugins/copilot.lua
+++ .config/nvim/lua/editor/plugins/copilot.lua
@@ -1,30 +1,61 @@
 return {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    build = ':Copilot auth',
-    opts = {
-        suggestion = {
-            enabled = true,
-            auto_trigger = true,
-            debounce = 75,
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        build = ':Copilot auth',
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+            },
+            panel = { enabled = false },
         },
-        panel = { enabled = false },
+        config = function(_, opts)
+            local utils = require 'core.utils'
+
+            local copilot = require 'copilot'
+            local copilot_api = require 'copilot.api'
+
+            copilot.setup(opts)
+
+            -- status updates for Copilot
+            local curr_status
+            copilot_api.register_status_notification_handler(function()
+                if copilot_api.status.data.status ~= curr_status then
+                    curr_status = copilot_api.status.data.status
+                    utils.trigger_status_update_event()
+                end
+            end)
+        end,
     },
-    config = function(_, opts)
-        local utils = require 'core.utils'
-
-        local copilot = require 'copilot'
-        local copilot_api = require 'copilot.api'
-
-        copilot.setup(opts)
-
-        -- status updates for Copilot
-        local curr_status
-        copilot_api.register_status_notification_handler(function()
-            if copilot_api.status.data.status ~= curr_status then
-                curr_status = copilot_api.status.data.status
-                utils.trigger_status_update_event()
-            end
-        end)
-    end,
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        branch = 'canary',
+        cmd = {
+            'CopilotChat',
+            'CopilotChatOpen',
+            'CopilotChatClose',
+            'CopilotChatToggle',
+            'CopilotChatStop',
+            'CopilotChatReset',
+            'CopilotChatSave',
+            'CopilotChatLoad',
+            'CopilotChatDebugInfo',
+            'CopilotChatExplain',
+            'CopilotChatReview',
+            'CopilotChatFix',
+            'CopilotChatOptimize',
+            'CopilotChatDocs',
+            'CopilotChatTests',
+            'CopilotChatFixDiagnostic',
+            'CopilotChatCommit',
+            'CopilotChatCommitStaged',
+        },
+        dependencies = {
+            'zbirenbaum/copilot.lua',
+            'nvim-lua/plenary.nvim',
+        },
+        opts = {},
+    },
 }
