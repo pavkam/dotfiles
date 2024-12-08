593bb96b40947f98e6239438d57c6b6ed4f23da2
Alexandru Ciobanu <alex+git@ciobanu.org>
2024-12-08 17:46:16 +0000

    chore: fix

--- .config/nvim/lua/api/buf.lua
+++ .config/nvim/lua/api/buf.lua
@@ -2,12 +2,21 @@
 ---@field window_ids integer[] # the window IDs that display this buffer.
 ---@field is_pinned_to_window fun(window_id: integer): boolean # whether the buffer is pinned to a window.
 
-local get_win_local_option_value = function(buffer_id, window_id)
+-- Get the value of a window-local option.
+---@param buffer_id integer # The buffer id.
+---@param window_id integer # The window id.
+---@param option string # The option name.
+local get_win_local_option_value = function(buffer_id, window_id, option)
+    xassert {
+        buffer_id = { buffer_id, { 'integer', ['>'] = 0 } },
+        window_id = { window_id, { 'integer', ['>'] = 0 } },
+        option = { option, { 'string', ['>'] = 0 } },
+    }
+
     local window_ids = vim.fn.getbufinfo(buffer_id)[1].windows
     for _, id in ipairs(window_ids) do
         if id == window_id then
-            local v = vim.api.nvim_get_option_value('winfixbuf', { win = window_id })
-            dbg('is_pinned_to_window', buffer_id, window_id, v)
+            local v = vim.api.nvim_get_option_value(option, { win = window_id })
             return v
         end
     end
@@ -30,7 +39,7 @@ local M = table.smart {
     },
     functions = {
         is_pinned_to_window = function(buffer_id, window_id)
-            return get_win_local_option_value(buffer_id, window_id)
+            return get_win_local_option_value(buffer_id, window_id, 'winfixbuf')
         end,
     },
 }
